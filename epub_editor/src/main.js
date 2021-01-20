a0c28a9f7aaba4c75aa5e2bdbfac4f67e28dd90b
Jubin Jang <sunshine0429@snu.ac.kr>
2021-01-20 11:48:04 +0900

    init : epub_editor project
    Vue.js 프로젝트를 생성하고 vuex, router, vuetify, electron 을 추가했다.

---
+++ epub_editor/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import store from './store'
+import router from './router'
+import vuetify from './plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
+
+Vue.config.productionTip = false
+
+new Vue({
+  store,
+  router,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app')
