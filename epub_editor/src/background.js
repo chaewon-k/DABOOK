5f89278a056a6c27c4b580e57c0a22e0d0866ca3
최나현 <cnnnnh@gmail.com>
2021-01-21 11:44:09 +0900

    add : mainpage
    Directory.vue, Preview.vue, Textarea.vue에 scroll, height 속성 추가
    background.js 에서 MenuBar 를 숨기고, height 속성을 변경하였다.

--- epub_editor/src/background.js
+++ epub_editor/src/background.js
@@ -14,7 +14,8 @@ async function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
     width: 800,
-    height: 600,
+    height: 700,
+    autoHideMenuBar: true,
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
