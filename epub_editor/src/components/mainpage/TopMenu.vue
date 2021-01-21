0be2be99c0042c897c82a953cbd6b70b125127a6
alsdk3586 <alsdk3586@hanmail.net>
2021-01-21 15:41:13 +0900

    init : topmenu
    TopMenu 수정
    SubMenu 생성 및 제작
    - 추가 기능 : TopMenu에서 값 클릭 시, 대응되는 SubMenu가 보인다.

--- epub_editor/src/components/mainpage/TopMenu.vue
+++ epub_editor/src/components/mainpage/TopMenu.vue
@@ -1,14 +1,53 @@
 <template>
-  <v-col>
-    <div style="border: 1px solid black;">
-      <h1>TopMenu</h1>
-    </div>
-  </v-col>
+  <div>
+    <!-- <v-system-bar>
+      <v-icon>mdi-message</v-icon>
+      <span>로고?</span>
+    </v-system-bar> -->
+    <v-card class="rounded-0">
+      <v-tabs
+        show-arrows
+        v-model="tab"
+      >
+        <v-tabs-slider></v-tabs-slider>
+        <v-tab
+          v-for="(item, idx) in items"
+          :key="idx"
+          @click="selectedIndex(idx)"
+        >
+          {{ item.tab }}
+        </v-tab>
+      </v-tabs>
+      <SubMenu
+        :itemIndex="itemIndex"
+      />
+    </v-card>
+  </div>
 </template>
-
 <script>
+import SubMenu from './SubMenu.vue'
 export default {
-
+  components: {
+    SubMenu
+  },
+  data () {
+      return {
+        itemIndex: 0,     //선택한 메뉴 인덱스
+        tab: null,
+        items: [
+          { tab: '파일' },
+          { tab: '편집' },
+          { tab: '창' },
+          { tab: '도구상자' },
+          { tab: '도움말' }
+        ],
+      }
+    },
+  methods: {
+    selectedIndex: function (idx) {
+      this.itemIndex = idx
+    }
+  }
 }
 </script>
 
