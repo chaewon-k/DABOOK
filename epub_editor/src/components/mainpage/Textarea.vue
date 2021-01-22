edc67abafbaa058b905455799b1cf4182e06e5d9
alsdk3586 <alsdk3586@hanmail.net>
2021-01-22 15:47:51 +0900

    add : mainpage
    textarea와 preview 연결

--- epub_editor/src/components/mainpage/Textarea.vue
+++ epub_editor/src/components/mainpage/Textarea.vue
@@ -4,6 +4,7 @@
     height="35em"
     label="textarea 입니다"
     placeholder="책 내용을 작성해볼까요?"
+    v-model="text"
   >
   </v-textarea>
 
@@ -11,8 +12,26 @@
 </template>
 
 <script>
-export default {
+import {mapMutations} from "vuex";
 
+export default {
+  data() {
+    return {
+      text:''
+    }
+  },
+  computed:{
+  },
+  methods:{
+    ...mapMutations(["SET_EDITINGTEXT"]),
+  },
+  watch:{
+    text:function(){
+      //console.log("watch");
+      //console.log(this.text);
+      this.SET_EDITINGTEXT(this.text);
+    }
+  }
 }
 </script>
 
