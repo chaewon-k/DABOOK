254c26aae90b3224ec70ce3ab382aee25d696620
DESKTOP-KVCQHCD\multicampus <cnh6123@naver.com>
2021-01-22 15:49:06 +0900

    edit : model 객체 naming 수정
    model 객체 naming ( customer -> user ) 수정

---
+++ epub_server/src/main/java/com/ssafy/epub/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ssafy.epub.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.ssafy.epub.model.User;
+
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
