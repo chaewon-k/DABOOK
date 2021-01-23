8945fb0bd6b493278303305bede51d7029fba393
DESKTOP-KVCQHCD\multicampus <cnh6123@naver.com>
2021-01-23 17:20:41 +0900

    edit : 회원 코드 일부 수정 및 이메일 인증 구현
    회원 Controller 일부 수정 및 회원 가입 이메일 인증 구현 완료하였습니다.

--- epub_server/src/main/java/com/ssafy/epub/model/User.java
+++ epub_server/src/main/java/com/ssafy/epub/model/User.java
@@ -1,6 +1,7 @@
 package com.ssafy.epub.model;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -23,12 +24,16 @@ public class User {
 	private String password;
 	private String nickname;
 	private boolean status;
-	
+	private String emailToken;
 	private List<String> epubList;
 	
 	public User(String email, String password, String nickname){
 		this.email = email;
 		this.password = password;
 		this.nickname = nickname;
+	}
+	
+	public void generateEmailToken() {
+		this.emailToken = UUID.randomUUID().toString();
 	}
 }
