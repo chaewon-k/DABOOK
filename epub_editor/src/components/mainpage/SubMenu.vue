90cc333de58cb7684d8703353247672033452b32
alsdk3586 <alsdk3586@hanmail.net>
2021-01-24 19:35:40 +0900

    add : mainpage
    submenu 편집 항목 추가
    - 취소하기, 되돌리기, 잘라내기, 복사하기, 봍여넣기, 찾기, 바꾸기 구현
    - 바꾸기 경우,
    	알파벳 구별없이 바꾸기 가능
    	모든 항목 바꾸기 가능

--- epub_editor/src/components/mainpage/SubMenu.vue
+++ epub_editor/src/components/mainpage/SubMenu.vue
@@ -8,13 +8,20 @@
       <v-btn text>e-pub으로 내보내기</v-btn>
     </template>
     <template v-else-if="itemIndex===1">
-      <v-btn text>취소하기</v-btn>
-      <v-btn text>되돌리기</v-btn>
-      <v-btn text>잘라내기</v-btn>
-      <v-btn text>복사하기</v-btn>
-      <v-btn text>붙여넣기</v-btn>
-      <v-btn text>찾기</v-btn>
-      <v-btn text>바꾸기</v-btn>
+
+      <v-btn @click="undo">취소하기</v-btn>
+      <v-btn @click="redo">되돌리기</v-btn>
+      <v-btn @click="cut">잘라내기</v-btn>
+      <v-btn @click="copy">복사하기</v-btn>
+      <v-btn @click="paste">붙여넣기</v-btn>
+      <v-btn @click="find">찾기</v-btn>
+      <v-btn @click="replace">바꾸기</v-btn>
+      <br>
+      찾을 단어 : <v-text-field clearable v-model="findText"></v-text-field>
+      바꿀 단어 : <v-text-field clearable v-model="replaceText"></v-text-field>
+      <v-checkbox v-model="replaceAlphabet" label="Aa"></v-checkbox>
+      <v-checkbox v-model="replaceAllText" label="All"></v-checkbox>
+       
     </template>
     <template v-else-if="itemIndex===2">
       <v-btn text>e-book 미리보기</v-btn>
@@ -40,16 +47,28 @@
 <script>
 import { readDirectory, test3 } from '@/functions/file.js'
 import eventBus from '@/eventBus.js'
+import { mapMutations, mapState } from 'vuex'
 const { dialog } = require('electron').remote
 
 export default {
   name: 'SubMenu',
   data() {
     return {
-      hTags: [1, 2, 3, 4, 5, 6]
+      hTags: [1, 2, 3, 4, 5, 6],
+      
+      /* itemIndex 1 : 편집 */
+      findText:'',
+      findIndexArray:[],
+      replaceText:'',
+      replaceAlphabet:false,
+      replaceAllText:false,
     }
   },
+  computed:{
+    ...mapState(["editingText"]),
+  },
   methods: {
+    ...mapMutations(["SET_EDITINGTEXT"]),
     selectHTag: function(index) {
       eventBus.$emit('pushIndexData', index);
     },
@@ -66,7 +85,77 @@ export default {
     },
     test: function () {
       test3()
-    }
+    },
+
+
+    /* 
+      < itemIndex 1 : 편집 >
+      cut/copy : 드래그 필수,
+      paste : chrome 환경에서 작동 안됨,
+    */
+    cut(){ //자르기
+      document.execCommand('cut');
+    },
+    copy(){ //복사
+      document.execCommand('copy');
+    },
+    paste(){ // 붙여넣기
+      document.execCommand('paste');
+    },
+    undo(){ // 실행 취소
+      document.execCommand('undo');
+    },
+    redo(){ // 실행 취소 되돌리기
+      document.execCommand('redo');
+    },
+    find(){ //찾기
+      this.findIndexArray=[];
+      let index=0;
+      console.log(this.editingText);
+      do{
+        index=this.editingText.indexOf(this.findText,index);
+        if(index==-1)
+          break;
+        this.findIndexArray.push(index);
+        index+=this.findText.length;
+      }while(index<this.editingText.length);
+      console.log(this.findIndexArray);
+    },
+    replace(){
+      find();
+      console.log("editingText: "+this.editingText);
+      console.log("findText:"+this.findText);
+      console.log("replaceText: "+this.replaceText);
+      
+      if(this.replaceAlphabet&&this.replaceAllText){
+        //all change and allCaseReplace
+        console.log("all change and all case replace");
+        let reqularExpression=new RegExp(this.findText,"gi");
+        let result=this.editingText.replace(reqularExpression,this.replaceText);
+        this.SET_EDITINGTEXT(result);
+      }
+      else if(this.replaceAlphabet){
+        //allCaseReplace
+        console.log("all case replace");
+        let reqularExpression=new RegExp(this.findData,"i");
+        let result=this.editingText.replace(reqularExpression,this.replaceText);
+        this.SET_EDITINGTEXT(result);
+      }
+      else if(this.replaceAllText){
+        //allchange
+        console.log("all change");
+        let reqularExpression=new RegExp(this.findText,"g");
+        let result=this.editingText.replace(reqularExpression,this.replaceText);
+        this.SET_EDITINGTEXT(result);
+      }
+      else{
+        //just one change
+        console.log("one change");
+        let result=this.editingText.replace(this.findText,this.replaceText);
+        this.SET_EDITINGTEXT(result);
+      }
+    },
+
   },
   props: {
     itemIndex: { type: Number }
@@ -75,5 +164,7 @@ export default {
 </script>
 
 <style>
-
+.v-text-field{
+  width: 150px;
+}
 </style>
