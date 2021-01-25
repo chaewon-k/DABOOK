ebc4f45d2b9ca5a339fe830283f71b75ee96f096
최나현 <cnnnnh@gmail.com>
2021-01-26 00:36:41 +0900

    add : epub 내보내기 구현
    경로 A의 e-book 폴더를 사용자가 입력한 title의 epub파일 형태로 경로 B에
    생성한다.
    경로는 임시적으로 하드코딩해둔 상태이다.
    
    경로 A : 'C:/Users/multicampus/Desktop/NewEbook'
    경로 B : 'C:/Users/multicampus/Desktop/'
    파일명 : title.epub

--- epub_editor/src/components/mainpage/SubMenu.vue
+++ epub_editor/src/components/mainpage/SubMenu.vue
@@ -5,7 +5,7 @@
       <v-btn @click="loadEbook" text>e-book 불러오기</v-btn>
       <v-btn text>저장하기</v-btn>
       <v-btn text>다른 이름으로 저장하기</v-btn>
-      <v-btn text>e-pub으로 내보내기</v-btn>
+      <v-btn @click.stop="titleDialog = true" text>e-pub으로 내보내기</v-btn>
     </template>
     <template v-else-if="itemIndex===1">
 
@@ -16,7 +16,7 @@
       <v-btn @click="paste">붙여넣기</v-btn>
       <v-btn @click="find">찾기</v-btn>
       <v-btn @click="replace">바꾸기</v-btn>
-      <br> 
+      <br>
       찾을 단어 : <v-text-field clearable v-model="findText"></v-text-field>
       바꿀 단어 : <v-text-field clearable v-model="replaceText"></v-text-field>
       <v-checkbox v-model="replaceAlphabet" label="Aa"></v-checkbox>
@@ -29,47 +29,11 @@
       <v-btn text>e-book 축소하기</v-btn>
     </template>
     <template v-else-if="itemIndex===3">
-      <div style="height: 100%">
-        <v-btn text x-large value="images">
-          <div>
-            <v-icon large class="d-flex justify-center" @click="selectImageTag()">mdi-image-search-outline</v-icon>
-            <p class="caption">Images</p>
-          </div>
-        </v-btn>
-        <v-btn text x-large>
-          <div>
-            <v-icon large @click="selectLink()">mdi-link-box-variant-outline</v-icon>
-            <p class="caption">Link</p>
-          </div>
-          </v-btn>
-        <v-btn text x-large>
-          <div>
-            <v-icon large @click="selectTable()">mdi-table-large-plus</v-icon>
-            <p class="caption">Table</p>
-          </div>
-        </v-btn>
-      </div>
-      <v-divider></v-divider>
-      <div>
-        <v-btn icon medium><v-icon medium @click="selectHTag(1)">mdi-format-header-1</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectHTag(2)">mdi-format-header-2</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectHTag(4)">mdi-format-header-4</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectHTag(3)">mdi-format-header-3</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectHTag(5)">mdi-format-header-5</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectHTag(6)">mdi-format-header-6</v-icon></v-btn>
-
-        <v-btn icon medium><v-icon medium @click="selectItalicTag()">mdi-format-italic</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectPTag()">mdi-format-paragraph</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectLineTag()">mdi-minus</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectBlockquoteTag()">mdi-comment-text-multiple-outline</v-icon></v-btn>
-        <v-btn icon medium text><v-icon medium @click="selectCiteTag()">mdi-comment-text-outline</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectBoldTag()">mdi-format-bold</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectUnderlineTag()">mdi-format-underline</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectMediumlineTag()">mdi-format-strikethrough</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectSubscriptTag()">mdi-format-letter-case-lower</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectSuperscriptTag()">mdi-format-letter-case-upper</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectUnorderedListTag()">mdi-format-list-bulleted</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectOrderedListTag()">mdi-format-list-numbered</v-icon></v-btn>
+      <div class="d-flex justify-start">
+        <div v-for="tag in hTags" :key="tag">
+          <v-btn @click="selectHTag(tag)">{{'h' + tag}}</v-btn>
+        </div>
+        <v-btn @click="selectItalicTag()">Italic</v-btn>
       </div>
     </template>
     <template v-else>
@@ -77,11 +41,31 @@
       <v-btn text>마크다운 설명서 보기</v-btn>
     </template>
 
+    <v-dialog v-model="titleDialog" max-width="290">
+      <v-card>
+        <v-card-title class="headline"> epub 이름을 입력해주세요. </v-card-title>
+        <v-card-text>
+          <v-text-field label="epub 이름" v-model="titleText" required></v-text-field>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="red darken-1" text @click="titleDialog = false">
+            취소
+          </v-btn>
+
+          <v-btn color="green darken-1" text @click="makeEpub">
+            epub 내보내기
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
   </div>
 </template>
 
 <script>
-import { readDirectory, tocToList } from '@/functions/file.js'
+import { readDirectory, tocToList, makeEpubFile } from '@/functions/file.js'
 import eventBus from '@/eventBus.js'
 import { mapMutations, mapState } from 'vuex'
 const fs = require('fs')
@@ -99,64 +83,21 @@ export default {
       replaceText:'',
       replaceAlphabet:false,
       replaceAllText:false,
+      titleText:'',
+      titleDialog:false,
     }
   },
-  computed:{
+  computed: {
     ...mapState(["editingText"]),
   },
   methods: {
     ...mapMutations(["SET_EDITINGTEXT"]),
-    //// 도구상자 모음
     selectHTag: function(index) {
       eventBus.$emit('pushIndexData', index);
     },
     selectItalicTag: function () {
       eventBus.$emit('pushIndexData', 'Italic');
     },
-    selectPTag: function () {
-      eventBus.$emit('pushIndexData', 'Enter');
-    },
-    selectLineTag: function () {
-      eventBus.$emit('pushIndexData', 'LineTag');
-    },
-    selectBlockquoteTag: function () {
-      eventBus.$emit('pushIndexData', 'BlockquoteTag');
-    },
-    selectCiteTag: function () {
-      eventBus.$emit('pushIndexData', 'CiteTag');
-    },
-    selectBoldTag: function () {
-      eventBus.$emit('pushIndexData', 'BoldTag');
-    },
-    selectUnderlineTag: function () {
-      eventBus.$emit('pushIndexData', 'UnderlineTag');
-    },
-    selectMediumlineTag: function () {
-      eventBus.$emit('pushIndexData', 'MediumlineTag');
-    },
-    selectSubscriptTag: function () {
-      eventBus.$emit('pushIndexData', 'SubscriptTag');
-    },
-    selectSuperscriptTag: function () {
-      eventBus.$emit('pushIndexData', 'SuperscriptTag');
-    },
-    selectImageTag: function () {
-      eventBus.$emit('pushIndexData', 'ImageTag');
-    },
-    selectUnorderedListTag: function () {
-      eventBus.$emit('pushIndexData', 'UnorderedListTag');
-    },
-    selectOrderedListTag: function () {
-      eventBus.$emit('pushIndexData', 'OrderedListTag');
-    },
-    selectLink: function () {
-      eventBus.$emit('pushIndexData', 'Link');
-    },
-    selectTable: function () {
-      eventBus.$emit('pushIndexData', 'Table');
-    },
-
-    //// 파일 모음
     loadEbook: function () { // E-BOOK 불러오기
       const options = {
         properties: ['openDirectory']
@@ -171,6 +112,11 @@ export default {
       const fileToText = fs.readFileSync(val[0]).toString()
       // console.log(tocToList(fileToText, []))
       this.$store.state.tableOfContents = tocToList(fileToText, [])
+    },
+    makeEpub: function () { // epub 내보내기
+      this.titleDialog = false; // title 적는 dialog 창 닫기
+      this.savePath = makeEpubFile(this.titleText); // file.js 안의 makeEpub 실행
+      this.titleText = '';
     },
     /* 
       < itemIndex 1 : 편집 >
