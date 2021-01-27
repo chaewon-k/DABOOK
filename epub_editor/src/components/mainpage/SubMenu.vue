039bc617dc15715fbc71e8ca9ee0963a0bd02083
chaewon <chechekk0@naver.com>
2021-01-28 00:04:32 +0900

    add : 파일생성 시 기본 이미지로 변경 & UI 디자인 적용
    - 파일 생성시 기본이미지 적용 버튼을 클릭하면 default image로 적용
    - mouse over, 기본 UI 디자인 변경

--- epub_editor/src/components/mainpage/SubMenu.vue
+++ epub_editor/src/components/mainpage/SubMenu.vue
@@ -1,118 +1,95 @@
 <template>
   <div>
     <template v-if="itemIndex===0">
-     
-        <v-dialog v-model="eBook" persistent max-width="600px">
-          <template v-slot:activator="{ on, attrs }">
-            <v-btn v-bind="attrs" v-on="on">새 e-book 생성</v-btn>
-          </template>
-          <v-card>
-            <v-card-title>
-              <span class="headline">E-Book 생성</span>
-            </v-card-title>
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col cols="6" sm="6" md="4">
-                    <v-text-field label="E-Book Title *" required v-model="eBookTitle"></v-text-field>
-                  </v-col>
-                </v-row>
-                <v-row>
-                  <v-col cols="12" sm="6" md="4">
-                    <div>
-                      <v-btn @click="load">위치 선택</v-btn>
-                      {{eBookLocation[0]}}
-                    </div>
-                  </v-col>
-                </v-row>
-                <v-row>
-                  <v-col cols="12" sm="6" md="7">
-                    <v-file-input
-                      v-model="eBookCover"
-                      accept="image/png, image/jpeg, image/bmp"
-                      prepend-icon="mdi-camera"
-                      label="E-Book Image"
-                    ></v-file-input>
-                  </v-col>
-                </v-row>
-              </v-container>
-              <small>*indicates required field</small>
-            </v-card-text>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="eBook = false">
-                Close
-              </v-btn>
-              <v-btn color="blue darken-1" text @click="createNewEBook">
-                Save
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-     
-
+      <v-dialog v-model="eBook" persistent max-width="600px">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn text v-bind="attrs" v-on="on">새 e-book 생성</v-btn>
+        </template>
+        <v-card>
+          <v-card-title>
+            <span class="headline">E-Book 생성</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container>
+              <v-row>
+                <v-col cols="6" sm="6" md="4">
+                  <v-text-field label="E-Book Title *" required v-model="eBookTitle"></v-text-field>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="12" sm="6" md="4">
+                  <div>
+                    <v-btn @click="load">위치 선택</v-btn>
+                    {{eBookLocation[0]}}
+                  </div>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="12" sm="6" md="7">
+                  <v-file-input
+                    v-model="eBookCover"
+                    accept="image/png, image/jpeg, image/bmp"
+                    prepend-icon="mdi-camera"
+                    label="E-Book Image"
+                  ></v-file-input>
+                  <v-btn @click="selectDefaultImg=true">기본 이미지로 저장</v-btn>
+                </v-col>
+              </v-row>
+            </v-container>
+            <small v-if="selectDefaultImg===true">기본 이미지를 선택</small>
+            <small v-else>*indicates required field</small>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" text @click="eBook = false">
+              Close
+            </v-btn>
+            <v-btn color="blue darken-1" text @click="createNewEBook">
+              Save
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
       <v-btn @click="loadEbook" text>e-book 불러오기</v-btn>
       <v-btn text @click="storeInputText">저장하기</v-btn>
+      <v-btn @click="preview" text>e-book 미리보기</v-btn>
       <!-- <v-btn text @click="storeNewInputText">다른 이름으로 저장하기</v-btn> -->
       <v-btn @click.stop="titleDialog = true" text>e-pub으로 내보내기</v-btn>
     </template>
     <template v-else-if="itemIndex===1">
-      <v-btn icon medium @click="undo"><v-icon medium>mdi-undo</v-icon></v-btn>
-      <v-btn icon medium @click="redo"><v-icon medium>mdi-redo</v-icon></v-btn>
-      <v-btn icon medium @click="cut"><v-icon medium>mdi-content-cut</v-icon></v-btn>
-      <v-btn icon medium @click="copy"><v-icon medium>mdi-content-copy</v-icon></v-btn>
-      <v-btn icon medium @click="paste"><v-icon medium>mdi-content-paste</v-icon></v-btn>
-      <v-btn icon medium @click.stop="findDialog = true" text><v-icon medium>mdi-file-find-outline</v-icon></v-btn>
-      <v-btn icon medium @click.stop="replaceDialog = true" text><v-icon medium>mdi-find-replace</v-icon></v-btn>
+      <v-tooltip bottom><template v-slot:activator="{ on }">
+        <v-btn v-on="on" icon medium @click="undo"><v-icon medium>mdi-undo</v-icon></v-btn></template>
+        <span>undo</span>
+      </v-tooltip>
+      <v-tooltip bottom><template v-slot:activator="{ on }">
+        <v-btn v-on="on" icon medium @click="redo"><v-icon medium>mdi-redo</v-icon></v-btn></template>
+        <span>redo</span>
+      </v-tooltip>
+      <v-tooltip bottom><template v-slot:activator="{ on }">
+        <v-btn v-on="on" icon medium @click="cut"><v-icon medium>mdi-content-cut</v-icon></v-btn></template>
+        <span>cut</span>
+      </v-tooltip>
+      <v-tooltip bottom><template v-slot:activator="{ on }">
+        <v-btn v-on="on" icon medium @click="copy"><v-icon medium>mdi-content-copy</v-icon></v-btn></template>
+        <span>copy</span>
+      </v-tooltip>
+      <v-tooltip bottom><template v-slot:activator="{ on }">
+        <v-btn v-on="on" icon medium @click="paste"><v-icon medium>mdi-content-paste</v-icon></v-btn></template>
+        <span>paste</span>
+      </v-tooltip>
+      <v-tooltip bottom><template v-slot:activator="{ on }">
+        <v-btn v-on="on" icon medium @click.stop="findDialog = true" text><v-icon medium>mdi-file-find-outline</v-icon></v-btn></template>
+        <span>findDialog</span>
+      </v-tooltip>
+      <v-tooltip bottom><template v-slot:activator="{ on }">
+        <v-btn v-on="on" icon medium @click.stop="replaceDialog = true" text><v-icon medium>mdi-find-replace</v-icon></v-btn></template>
+        <span>replaceDialog</span>
+      </v-tooltip>
     </template>
     <template v-else-if="itemIndex===2">
-      <v-btn @click="preview" text>e-book 미리보기</v-btn>
-      <v-btn text>e-book 확대하기</v-btn>
-      <v-btn text>e-book 축소하기</v-btn>
     </template>
     <template v-else-if="itemIndex===3">
-      <div style="height: 100%">
-        <v-btn text x-large value="images">
-          <div>
-            <v-icon large class="d-flex justify-center" @click="selectImageTag()">mdi-image-search-outline</v-icon>
-            <p class="caption">Images</p>
-          </div>
-        </v-btn>
-        <v-btn text x-large>
-          <div>
-            <v-icon large @click="selectLink()">mdi-link-box-variant-outline</v-icon>
-            <p class="caption">Link</p>
-          </div>
-          </v-btn>
-        <v-btn text x-large>
-          <div>
-            <v-icon large @click="selectTable()">mdi-table-large-plus</v-icon>
-            <p class="caption">Table</p>
-          </div>
-        </v-btn>
-      </div>
-      <v-divider></v-divider>
-      <div>
-        <v-btn icon medium><v-icon medium @click="selectHTag(1)">mdi-format-header-1</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectHTag(2)">mdi-format-header-2</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectHTag(3)">mdi-format-header-3</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectHTag(4)">mdi-format-header-4</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectHTag(5)">mdi-format-header-5</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectHTag(6)">mdi-format-header-6</v-icon></v-btn>
-
-        <v-btn icon medium><v-icon medium @click="selectItalicTag()">mdi-format-italic</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectPTag()">mdi-format-paragraph</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectLineTag()">mdi-minus</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectBlockquoteTag()">mdi-comment-text-multiple-outline</v-icon></v-btn>
-        <v-btn icon medium text><v-icon medium @click="selectCiteTag()">mdi-comment-text-outline</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectBoldTag()">mdi-format-bold</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectUnderlineTag()">mdi-format-underline</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectMediumlineTag()">mdi-format-strikethrough</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectSubscriptTag()">mdi-format-letter-case-lower</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectSuperscriptTag()">mdi-format-letter-case-upper</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectUnorderedListTag()">mdi-format-list-bulleted</v-icon></v-btn>
-        <v-btn icon medium><v-icon medium @click="selectOrderedListTag()">mdi-format-list-numbered</v-icon></v-btn>
-      </div>
+      <ToolsTab/>
     </template>
     <template v-else>
       <v-btn text>editor 사용 설명서 보기</v-btn>
@@ -213,6 +190,8 @@
 import { readDirectory, tocToList, makeEpubFile, addContentOpf, addTocNcx } from '@/functions/file.js'
 import eventBus from '@/eventBus.js'
 import { mapMutations, mapState } from 'vuex'
+import ToolsTab from '@/components/mainpage/menutabs/ToolsTab'
+
 const fs = require('fs')
 const path=require('path')
 const electron = require('electron')
@@ -222,6 +201,9 @@ const BrowserWindow = electron.remote.BrowserWindow
 const fse=require('fs-extra')
 export default {
   name: 'SubMenu',
+  components: {
+    ToolsTab
+  },
   data() {
     return {
       hTags: [1, 2, 3, 4, 5, 6],
@@ -236,6 +218,7 @@ export default {
       titleDialog:false,
       findDialog: false,
       replaceDialog: false,
+      selectDefaultImg: false,
 
       /* itemIndex 0 :  */
       eBook:false, // 새 eBook 만들기 Dialog 활성화/비활성화
@@ -264,57 +247,6 @@ export default {
       const updatedText = this.$store.state.editingHTMLText + this.$store.state.editingText + '</html>'
       fs.writeFileSync(this.$store.state.selectedFileDirectory, updatedText)
     },
-
-    // 도구상자 모음 탭
-    selectHTag: function(index) {
-      eventBus.$emit('pushIndexData', index);
-    },
-    selectItalicTag: function () {
-      eventBus.$emit('pushIndexData', 'Italic');
-    },
-    selectPTag: function () {
-      eventBus.$emit('pushIndexData', 'Enter');
-    },
-    selectLineTag: function () {
-      eventBus.$emit('pushIndexData', 'LineTag');
-    },
-    selectBlockquoteTag: function () {
-      eventBus.$emit('pushIndexData', 'BlockquoteTag');
-    },
-    selectCiteTag: function () {
-      eventBus.$emit('pushIndexData', 'CiteTag');
-    },
-    selectBoldTag: function () {
-      eventBus.$emit('pushIndexData', 'BoldTag');
-    },
-    selectUnderlineTag: function () {
-      eventBus.$emit('pushIndexData', 'UnderlineTag');
-    },
-    selectMediumlineTag: function () {
-      eventBus.$emit('pushIndexData', 'MediumlineTag');
-    },
-    selectSubscriptTag: function () {
-      eventBus.$emit('pushIndexData', 'SubscriptTag');
-    },
-    selectSuperscriptTag: function () {
-      eventBus.$emit('pushIndexData', 'SuperscriptTag');
-    },
-    selectImageTag: function () {
-      eventBus.$emit('pushIndexData', 'ImageTag');
-    },
-    selectUnorderedListTag: function () {
-      eventBus.$emit('pushIndexData', 'UnorderedListTag');
-    },
-    selectOrderedListTag: function () {
-      eventBus.$emit('pushIndexData', 'OrderedListTag');
-    },
-    selectLink: function () {
-      eventBus.$emit('pushIndexData', 'Link');
-    },
-    selectTable: function () {
-      eventBus.$emit('pushIndexData', 'Table');
-    },
-
 
     /* 새 ebook 만들기 */
     load: function(){
@@ -363,12 +295,15 @@ export default {
        새 ebook 만들기 
        - 표지 이미지 파일 EPUB 폴더 내 image 폴더로 파일로 복사
         */
-      let coverLocation=path.resolve(this.eBookLocation+'/EPUB/images/'+this.eBookCover.name); //이미지 저장할 위치 
-      this.dirAndFileCopy(this.eBookCover.path,coverLocation); // 입력받은 이미지를 저장할 위치로 복사
-
-      this.renameImageTag(this.eBookLocation);
+      if (this.selectDefaultImg === true) {   // 기본 이미지를 선택할 경우
+        this.eBookCover.name = 'default.jpg'
+        this.selectDefaultImg = false
+      } else {  // 이미지를 선택할 경우
+        let coverLocation=path.resolve(this.eBookLocation+'/EPUB/images/'+this.eBookCover.name); //이미지 저장할 위치 
+        this.dirAndFileCopy(this.eBookCover.path,coverLocation); // 입력받은 이미지를 저장할 위치로 복사
+      }
+      this.renameImageTag();
       this.eBook=false;
-
       this.readToc(this.eBookLocation);
     },
     readToc(eBookLocation){
@@ -377,9 +312,9 @@ export default {
       console.log(data);
       this.getToc(data['toc'])
     },
-    renameImageTag: function (eBookLocation) {
-      let coverLocation=path.resolve(eBookLocation+'/EPUB/images/'+this.eBookCover.name);
-      let newCoverLocation=path.resolve(eBookLocation+'/EPUB/images/cover.png');
+    renameImageTag: function () {
+      let coverLocation=path.resolve(this.eBookLocation+'/EPUB/images/'+this.eBookCover.name);
+      let newCoverLocation=path.resolve(this.eBookLocation+'/EPUB/images/cover.png');
       fs.rename(coverLocation, newCoverLocation, function(err){
       if( err ) throw err;
       console.log('File Renamed!');
