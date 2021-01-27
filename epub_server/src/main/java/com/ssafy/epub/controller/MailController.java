c4e90bcba71fd50678fb1cbcedd543bf6cbc866c
DESKTOP-KVCQHCD\multicampus <dhpassion@naver.com>
2021-01-27 14:46:45 +0900

    add : add code comment
    Controller & JWT token & MongoDB Code에 관한 Comment 추가.

--- epub_server/src/main/java/com/ssafy/epub/controller/MailController.java
+++ epub_server/src/main/java/com/ssafy/epub/controller/MailController.java
@@ -29,15 +29,17 @@ public class MailController {
 
 	private EncryptHandler encryptHandler = new BcryptImpl();
 
+	// 이메일 인증
 	@GetMapping("/auth")
 	@ApiOperation(value = "verify email", produces = MediaType.TEXT_PLAIN_VALUE)
 	public ResponseEntity<Boolean> verifyEmail(String token, String email) {
 		try {
-			List<User> findUserList = userRepository.findByEmail(email);
-			if (findUserList.size() == 1 && findUserList.get(0).getEmailToken().equals(token)) {
-				User preUser = findUserList.get(0);
-				preUser.setStatus(true);
-				userRepository.save(preUser);
+			// findByEmail의 반환이 List & 회원가입 시 중복체크를 함으로 email에 맞는 User는 한명이다.
+			User findUser = userRepository.findByEmail(email).get(0);
+			if (findUser.getEmailToken().equals(token)) {
+				// 이메일 인증 완료시 Status를 false에서 true로 전환한다.
+				findUser.setStatus(true);
+				userRepository.save(findUser);
 				return new ResponseEntity<>(true, HttpStatus.OK);
 			} else
 				return new ResponseEntity<>(false, HttpStatus.NO_CONTENT);
@@ -47,7 +49,7 @@ public class MailController {
 
 		}
 	}
-	
+	// 비밀번호 찾기.
 	@GetMapping("/password")
 	@ApiOperation(value = "find password", produces = MediaType.TEXT_PLAIN_VALUE)
 	public ResponseEntity<Boolean> findPassword(String email) {
@@ -55,10 +57,14 @@ public class MailController {
 		
 		try {
 			List<User> findUserList = userRepository.findByEmail(email);
+			// User가 입력한 이메일이 DB에 없는 경우를 체크
 			if (findUserList.size() == 1) {
 				User preUser = findUserList.get(0);
+				// 비밀번호는 임시 비밀번호로 발송한다.
 				mailService.findPasswordEmailSender(preUser, tempPassword);
+				// 비밀번호는 사용자만 알고있어야함으로 DB에는 암호화되어 저장된다.
 				preUser.setPassword(encryptHandler.encrypt(tempPassword));
+				// 바뀐 임시비밀번호로 저장.
 				userRepository.save(preUser);
 				return new ResponseEntity<>(true, HttpStatus.OK);
 			} else
@@ -69,7 +75,7 @@ public class MailController {
 
 		}
 	}
-	
+	// 임시 비밀번호 만들기.
 	private String getTempPassword(){
         char[] charSet = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
                 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' ,
