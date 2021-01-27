e6aa4e2bdfc2562907bbf48d79111ac5a834f839
DESKTOP-KVCQHCD\multicampus <dhpassion@naver.com>
2021-01-27 14:54:21 +0900

    edit : Bcrypt comment add
    Bcrypt comment 추가 및 Sample Controller 삭제

--- epub_server/src/main/java/com/ssafy/epub/encrypt/BcryptImpl.java
+++ epub_server/src/main/java/com/ssafy/epub/encrypt/BcryptImpl.java
@@ -5,12 +5,13 @@ import org.mindrot.jbcrypt.BCrypt;
 
 public class BcryptImpl implements EncryptHandler{
 
+	// 사용자의 비밀번호를 암호화한다.
 	@Override
 	public String encrypt(String password) {
 		
 		return BCrypt.hashpw(password, BCrypt.gensalt());
 	}
-
+	// 사용자가 입력한 비밀번호와 암호화된 비밀번호를 매칭하여 검사한다.
 	@Override
 	public boolean isMatch(String password, String hashed) {
 		
