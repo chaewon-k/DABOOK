c63aa984e92add7e77bbe5f4598c4c9e237a0ef9
chaewon <chechekk0@naver.com>
2021-02-01 18:22:26 +0900

    init : 로그인,회원가입 페이지 생성 & server 연동
    로그인, 회원가입 페이지를 생성하고, axios를 통해 server와 연동시켰다.
    
    
    Former-commit-id: bf2879aa4eafe3702a5a0cd4a42e5192c08fdd93

--- epub_editor/src/views/Login.vue
+++ epub_editor/src/views/Login.vue
@@ -16,7 +16,6 @@
               type="password"
               v-model="inputPassword"
             ></v-text-field>
-
             <div class="float-right text-primary">
               <p>비밀번호를 잊으셨나요?</p>
             </div>
@@ -25,19 +24,40 @@
       </v-window>
       <v-divider></v-divider>
       <v-card-actions>
-        <v-btn>LOGIN</v-btn>
-        <v-btn>SIGNUP</v-btn>
+        <v-btn @click="logIn">LOGIN</v-btn>
+        <v-btn @click="signUp">SIGNUP</v-btn>
       </v-card-actions>
     </v-card>
   </div>
 </template>
 <script>
+  import axios from 'axios'
   export default {
     name: 'Login',
     data: function () {
       return {
         inputEmail: '',
         inputPassword: ''
+      }
+    },
+    methods: {
+      logIn: function () {
+        const data = {"email" : this.inputEmail, "password" : this.inputPassword}
+        axios.post("http://i4a103.p.ssafy.io:8080/api/login", data)
+          .then(res => {
+            const message = res.data.result
+            if (message === "ERROR_ID"){console.log(message)}
+            else if (message === "ERROR_STATUS"){console.log(message)}
+            else if (message === "ERROR_PASSWORD"){console.log(message)}
+            else {
+              console.log(message)
+              this.$router.push({ name: 'Editor'})
+            }
+          })
+          .catch(err => console.log(err))
+      },
+      signUp: function () {
+        this.$router.push({ name: 'Signup'})
       }
     }
   }
