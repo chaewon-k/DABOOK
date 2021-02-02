1132d70979c260512af44e655c942d5914aa547a
chaewon <chechekk0@naver.com>
2021-02-02 16:49:28 +0900

    add : 로그인 & 회원가입 세부기능 추가
    로그인
    - 이메일 인증 완료 상태 확인 후 로그인
    
    회원가입
    - 이메일 기본 규칙 (rules)을 추가했다.
    - 이메일 인증 기능을 추가했다.
    - 인증메일 수신 확인 후 서버 응답에 따라 대응하는 함수를 추가했다.
    
    
    Former-commit-id: e95e358a460b66644483b9dc3d0d5d68345d159a

--- epub_editor/src/views/Login.vue
+++ epub_editor/src/views/Login.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <!---------------Alert 창-------------------->
+    <v-alert outlined dense icon="mdi-alert-circle-outline" :color="type" v-if="type">
+      {{ message }}
+    </v-alert>
+    <!---------------login form------------------>
     <v-card class="ma-auto" max-width="500">
       <v-card-title class="title font-weight-regular justify-space-between">
         <span>Login</span>
@@ -20,6 +25,7 @@
             ></v-text-field>
           </v-row>
         </v-container>
+        <!-------------비밀번호 찾기 ----------------->
         <div class="float-right">
           <p>비밀번호를 잊으셨나요?</p>
         </div>
@@ -38,19 +44,63 @@
     name: 'Login',
     data: function () {
       return {
+        // 서버에 보낼 데이터
         inputEmail: '',
-        inputPassword: ''
+        inputPassword: '',
+
+        // Alert 용 데이터
+        message: '',
+        type: null,
+        elapse:null,
       }
     },
     methods: {
+      // 3초 후 사라지는 alert 구현 함수
+      showAlert (type) {
+        this.type = type
+        let timer = this.showAlert.timer
+        if (timer) {
+          clearTimeout(timer)
+        }
+        this.showAlert.timer = setTimeout(() => {
+            this.type = null
+        }, 3000)
+        
+        this.elapse = 1 
+        let t = this.showAlert.t
+        if (t) {
+          clearInterval(t)
+        }
+        
+        this.showAlert.t = setInterval(() => {
+          if (this.elapse === 3) {
+            this.elapse = 0
+            clearInterval(this.showAlert.t)
+          }
+          else {
+            this.elapse++
+          }
+        }, 1000)
+      },
+
+      // 로그인 
       logIn: function () {
         const data = {"email" : this.inputEmail, "password" : this.inputPassword}
         axios.post("https://i4a103.p.ssafy.io/api/login", data)
           .then(res => {
             const message = res.data.result
-            if (message === "ERROR_ID"){console.log(message)}
-            else if (message === "ERROR_STATUS"){console.log(message)}
-            else if (message === "ERROR_PASSWORD"){console.log(message)}
+            if (message === "ERROR_ID"){
+              this.message = '존재하지 않는 ID입니다.'
+              this.showAlert('error')
+            }
+            else if (message === "ERROR_STATUS"){
+              this.message = '이메일 인증을 해주세요.'
+              this.showAlert('error')
+            }
+            else if (message === "ERROR_PASSWORD"){
+              this.message = '존재하지 않는 비밀번호입니다.'
+              this.showAlert('error')
+            }
             else {
               console.log(message)
               this.$router.push({ name: 'Editor'})
@@ -58,6 +108,8 @@
           })
           .catch(err => console.log(err))
       },
+
+      // 회원가입 창으로 이동
       signUp: function () {
         this.$router.push({ name: 'Signup'})
       }
