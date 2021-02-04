8d51b23b737924797e156157fc6255e1a7f05bb4
chaewon <chechekk0@naver.com>
2021-02-04 16:42:33 +0900

    add :[협업] Google Login with Electron
    - 구글의 보안 정책이 강화로 일반 google로그인 api 대신 @getstation/electron-google-oauth2 를 설치하여 Access Token과정에 접근했다.
    - client와 구글의 통신 부분을 구현했다. id_token과 access_token을 발급받는
    것에 성공했다.
    - typescript를 사용했다.
    (강세준, 최낙훈, 강채원 협업)
    
    
    Former-commit-id: 1dbc69ce8eeab68821353a42d2d1c19544b3ce6d

--- epub_editor/src/views/Login.vue
+++ epub_editor/src/views/Login.vue
@@ -36,101 +36,102 @@
         <v-card-actions>
           <v-btn @click="logIn">LOGIN</v-btn>
           <v-btn @click="signUp">SIGNUP</v-btn>
+          <router-link :to="{name:'GoogleLogin'}"><v-btn>Google</v-btn></router-link>
         </v-card-actions>
       </v-card>
     </v-layout>
   </v-container>
 </template>
 <script>
-  import axios from 'axios'
-  export default {
-    name: 'Login',
-    data: function () {
-      return {
-        // 서버에 보낼 데이터
-        inputEmail: '',
-        inputPassword: '',
+import axios from 'axios'
+export default {
+  name: 'Login',
+  data: function () {
+    return {
+      // 서버에 보낼 데이터
+      inputEmail: '',
+      inputPassword: '',
 
-        // Alert 용 데이터
-        message: '',
-        type: null,
-        elapse:null,
+      // Alert 용 데이터
+      message: '',
+      type: null,
+      elapse:null,
+    }
+  },
+  methods: {
+    // 3초 후 사라지는 alert 구현 함수
+    showAlert (type) {
+      this.type = type
+      let timer = this.showAlert.timer
+      if (timer) {
+        clearTimeout(timer)
       }
+      this.showAlert.timer = setTimeout(() => {
+          this.type = null
+      }, 3000)
+      
+      this.elapse = 1 
+      let t = this.showAlert.t
+      if (t) {
+        clearInterval(t)
+      }
+      
+      this.showAlert.t = setInterval(() => {
+        if (this.elapse === 3) {
+          this.elapse = 0
+          clearInterval(this.showAlert.t)
+        }
+        else {
+          this.elapse++
+        }
+      }, 1000)
     },
-    methods: {
-      // 3초 후 사라지는 alert 구현 함수
-      showAlert (type) {
-        this.type = type
-        let timer = this.showAlert.timer
-        if (timer) {
-          clearTimeout(timer)
-        }
-        this.showAlert.timer = setTimeout(() => {
-            this.type = null
-        }, 3000)
-        
-        this.elapse = 1 
-        let t = this.showAlert.t
-        if (t) {
-          clearInterval(t)
-        }
-        
-        this.showAlert.t = setInterval(() => {
-          if (this.elapse === 3) {
-            this.elapse = 0
-            clearInterval(this.showAlert.t)
+
+    // 로그인 
+    logIn: function () {
+      const data = {"email" : this.inputEmail, "password" : this.inputPassword}
+      axios.post("https://i4a103.p.ssafy.io/api/login", data)
+        .then(res => {
+          const message = res.data.result
+          if (message === "ERROR_ID"){
+            this.message = '존재하지 않는 ID입니다.'
+            this.showAlert('error')
+          }
+          else if (message === "ERROR_STATUS"){
+            this.message = '이메일 인증을 해주세요.'
+            this.showAlert('error')
+          }
+          else if (message === "ERROR_PASSWORD"){
+            this.message = '존재하지 않는 비밀번호입니다.'
+            this.showAlert('error')
           }
           else {
-            this.elapse++
-          }
-        }, 1000)
-      },
-
-      // 로그인 
-      logIn: function () {
-        const data = {"email" : this.inputEmail, "password" : this.inputPassword}
-        axios.post("https://i4a103.p.ssafy.io/api/login", data)
-          .then(res => {
-            const message = res.data.result
-            if (message === "ERROR_ID"){
-              this.message = '존재하지 않는 ID입니다.'
-              this.showAlert('error')
-            }
-            else if (message === "ERROR_STATUS"){
-              this.message = '이메일 인증을 해주세요.'
-              this.showAlert('error')
-            }
-            else if (message === "ERROR_PASSWORD"){
-              this.message = '존재하지 않는 비밀번호입니다.'
-              this.showAlert('error')
-            }
-            else {
-              console.log(res)
-              // local Storage에 로그인 정보 저장.
-              localStorage.setItem('token',res.data.token)
-              localStorage.setItem('email', res.data.email)
-              localStorage.setItem('nickname', res.data.nickname)
-              this.$router.push({ name: 'Editor'})
-            }
-          })
-          .catch(err => console.log(err))
-      },
-
-      // 회원가입 창으로 이동
-      signUp: function () {
-        this.$router.push({ name: 'Signup'})
-      },
-
-      // 임시 비밀번호 발급
-      findPassword: function () {
-        axios.get(`https://i4a103.p.ssafy.io/password?email=${this.inputEmail}`)
-          .then(res =>{
             console.log(res)
-          })
-          .catch(err => console.log(err))
-        this.message = '해당 이메일로 임시 비밀번호를 전송했습니다.'
-        this.showAlert('purple')
-      }
+            // local Storage에 로그인 정보 저장.
+            localStorage.setItem('token',res.data.token)
+            localStorage.setItem('email', res.data.email)
+            localStorage.setItem('nickname', res.data.nickname)
+            this.$router.push({ name: 'Editor'})
+          }
+        })
+        .catch(err => console.log(err))
+    },
+
+    // 회원가입 창으로 이동
+    signUp: function () {
+      this.$router.push({ name: 'Signup'})
+    },
+
+    // 임시 비밀번호 발급
+    findPassword: function () {
+      axios.get(`https://i4a103.p.ssafy.io/password?email=${this.inputEmail}`)
+        .then(res =>{
+          console.log(res)
+        })
+        .catch(err => console.log(err))
+      this.message = '해당 이메일로 임시 비밀번호를 전송했습니다.'
+      this.showAlert('purple')
     }
   }
+}
 </script>
