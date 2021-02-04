65d0b1b260f9c9bb40ac4a2c81442b44e944ac3a
강세준 <dhpassion@naver.com>
2021-02-04 11:51:28 +0900

    add : Google login Oauth
    Google login 구현중
    
    
    Former-commit-id: c810a0e1097ed8b31f77cfe3c0f52e834a23d5e7

--- epub_server/src/main/java/com/ssafy/epub/EpubServerApplication.java
+++ epub_server/src/main/java/com/ssafy/epub/EpubServerApplication.java
@@ -2,9 +2,11 @@ package com.ssafy.epub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 //@EnableMongoRepositories("com.ssafy.epub.repository")
 //(exclude = {DataSourceAutoConfiguration.class})
+@EnableConfigurationProperties
 @SpringBootApplication
 public class EpubServerApplication{
 	
