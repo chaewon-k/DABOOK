d6f4af6602c8a5c44dd5b6cab29ce074a775d90c
chaewon <chechekk0@naver.com>
2021-02-04 16:42:33 +0900

    add : Google Login with Electron
    - 구글의 보안 정책이 강화로 일반 google로그인 api 대신 @getstation/electron-google-oauth2 를 설치하여 Access Token과정에 접근했다.
    - client와 구글의 통신 부분을 구현했다. id_token과 access_token을 발급받는
    것에 성공했다.
    - typescript를 사용했다.
    
    
    Former-commit-id: 934a534e165ca9059375517ac74f1babb9ea716b

--- epub_editor/src/main.js
+++ epub_editor/src/main.js
@@ -5,8 +5,16 @@ import router from './router'
 import vuetify from './plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
+// import GAuth from 'vue-google-oauth2'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
+
+// const gauthOption = {
+//   clientId: '613406129704-omk18m4bqaei962v5s2hsn654cj2den2.apps.googleusercontent.com',
+//   scope: 'profile email'
+// }
+
+// Vue.use(GAuth, gauthOption)
 
 new Vue({
   store,
