c2a36c42435df8b84ec4fc51409582978fb55d25
강세준 <dhpassion@naver.com>
2021-02-07 07:38:01 +0000

    Revert "Merge branch 'f/s-server' into 'develop'"
    This reverts merge request !11
    
    Former-commit-id: cc14a762a0c20d8a21456bb55e8a80b4b9dc736d

--- epub_server/src/main/java/com/ssafy/epub/EpubServerApplication.java
+++ epub_server/src/main/java/com/ssafy/epub/EpubServerApplication.java
@@ -2,9 +2,8 @@ package com.ssafy.epub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@EnableMongoRepositories(basePackages = "com.ssafy.epub.repository")
+//@EnableMongoRepositories("com.ssafy.epub.repository")
 //(exclude = {DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class EpubServerApplication{
