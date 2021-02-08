70914ef9fe87a3ce3002c8f883bbc3531b6c9e99
Jubin Jang <sunshine0429@snu.ac.kr>
2021-02-09 00:10:30 +0900

    add : build configuration 추가
    - 빌드를 위한 설정을 vue.config.js 파일과 package.json 에 추가하였다.

--- epub_editor/vue.config.js
+++ epub_editor/vue.config.js
@@ -6,21 +6,30 @@ module.exports = {
     electronBuilder: {
       nodeIntegration: true,
       nodeModulesPath: ['../../node_modules', './node_modules'],
-
       builderOptions: {
-        appId: 'com.ssafy.dabook',
+        appId: 'com.dabook.app',
         productName: 'dabook',
-        asar: false,
+        asar: true,
         protocols: {
           name: 'dabook',
           schemes: [
             'dabook'
           ],
         },
+        mas: {
+          hardenedRuntime: false, //IMPORTANT!!!!
+          type: "distribution",
+          category: "public.app-category.utilities",
+          entitlements: "build/entitlements.mas.plist",
+          entitlementsInherit: "build/entitlements.mas.inherit.plist"
+        },
         mac: {
-          category: 'public.app-category.productivity',
-          // target: 'mas',
-          // identity: 'String - The name of certificate to use when signing. Consider using environment variables CSC_LINK or CSC_NAME instead of specifying this option. MAS installer identity is specified in the mas.',
+          category: 'public.app-category.utilities',
+          target: 'mas',
+          cscLink: "build/all-certs.p12",//'String - The name of certificate to use when signing. Consider using environment variables CSC_LINK or CSC_NAME instead of specifying this option. MAS installer identity is specified in the mas.',
+          cscKeyPassword : "skrgnswnqls@123",
+          icon: "build/MyIcon.icns",
+          provisioningProfile : "build/dabook.provisionprofile",
         },
       },
     }
