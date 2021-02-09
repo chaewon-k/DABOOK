69b82c83fb6ca450ea31476d6b9431524d915bd7
최나현 <cnnnnh@gmail.com>
2021-02-09 20:27:50 +0900

    edit : dialog UI 설계 및 컴포넌트화 & 태그 들여쓰기 수정
    - dialog 통일감을 위해 UI를 재설계하였다.
    - 재사용성을 높이기 위해 자주 사용하는 dialog를 컴포넌트별로 분할하였다.
    - style tag의 들여쓰기를 재점검하였다.
    
    
    Former-commit-id: fe77bbf70bfb6895bb57ce2c0bb172d8580a6f8d

---
+++ epub_editor/src/components/Dialog/DialogButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card-actions style="padding: 1% 6% 5% 6%">
+    <v-spacer></v-spacer>
+    <v-btn color="primary" @click="dialogMethod()">{{ buttonText }}</v-btn>
+  </v-card-actions>
+</template>
+
+<script>
+export default {
+  name: "DialogButton",
+  props: ["buttonText", "dialogMethod"],
+  methods: {},
+};
+</script>
+
+<style></style>
