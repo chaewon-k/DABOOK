3fd1f8a58ce059223418c7c770854007fc5ca203
Jubin Jang <sunshine0429@snu.ac.kr>
2021-02-09 13:58:23 +0900

    edit : 상단 메뉴바 추가
    - 상단 메뉴바 틀 추가
    
    
    Former-commit-id: 3ddc4dcdecd97486cebf2effacfe1cae50c06061

--- epub_editor/src/background.js
+++ epub_editor/src/background.js
@@ -4,20 +4,310 @@ import { app, protocol, Menu, BrowserWindow } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 const isDevelopment = process.env.NODE_ENV !== 'production'
+// const { ipcMain } = require('electron')
+// const { webContents } = require('electron')
+// let menuMessage = 0
+
 
 const template = [
   {
-    label: 'Filter',
+    label: '파일',
     submenu: [
       {
-        label: 'Hello',
-        accelerator: 'Shift+CmdOrCtrl+H',
-        click() {
-            console.log('Oh, hi there!')
+        label: '이북 생성하기',
+        click: function () {
+          console.log('이북 생성하기')
         }
-      }
-    ]
-  }
+      },
+      {
+        label: '이북 불러오기',
+        click: function () {
+          console.log('이북 불러오기')
+        }
+      },
+      {
+        label: '저장하기',
+        click: function () {
+          console.log('저장하기')
+        }
+      },
+      {
+        label: '이북 미리보기',
+        click: function () {
+          console.log('이북 미리보기')
+        }
+      },
+      {
+        label: 'ePub으로 내보내기',
+        click: function () {
+          console.log('ePub으로 내보내기')
+        }
+      },
+      {
+        label: '목차 추가하기',
+        click: function () {
+          console.log('목차 추가하기')
+        }
+      },
+    ],
+  },
+  {
+    label: '편집',
+    submenu: [
+      {
+        label: '실행취소',
+        click: function () {
+          console.log('실행취소')
+        }
+      },
+      {
+        label: '되돌리기',
+        click: function () {
+          console.log('되돌리기')
+        }
+      },
+      {
+        label: '잘라내기',
+        click: function () {
+          console.log('잘라내기')
+        }
+      },
+      {
+        label: '복사하기',
+        click: function () {
+          console.log('복사하기')
+        }
+      },
+      {
+        label: '붙여넣기',
+        click: function () {
+          console.log('붙여넣기')
+        }
+      },
+      {
+        label: '찾기',
+        click: function () {
+          console.log('찾기')
+        }
+      },
+      {
+        label: '바꾸기',
+        click: function () {
+          console.log('바꾸기')
+        }
+      },
+    ],
+  },
+  {
+    label: '도구상자',
+    submenu: [
+      {
+        label: '제목1',
+        click: function () {
+          console.log('제목1')
+        }
+      },
+      {
+        label: '제목2',
+        click: function () {
+          console.log('제목2')
+        }
+      },
+      {
+        label: '제목3',
+        click: function () {
+          console.log('제목3')
+        }
+      },
+      {
+        label: '제목4',
+        click: function () {
+          console.log('제목4')
+        }
+      },
+      {
+        label: '제목5',
+        click: function () {
+          console.log('제목5')
+        }
+      },
+      {
+        label: '제목6',
+        click: function () {
+          console.log('제목6')
+        }
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: '이탤릭체',
+        click: function () {
+          console.log('이탤릭체')
+        }
+      },
+      {
+        label: '줄 바꿈',
+        click: function () {
+          console.log('줄 바꿈')
+        }
+      },
+      {
+        label: '선',
+        click: function () {
+          console.log('선')
+        }
+      },
+      {
+        label: '인용구',
+        click: function () {
+          console.log('인용구')
+        }
+      },
+      {
+        label: '인용문',
+        click: function () {
+          console.log('인용문')
+        }
+      },
+      {
+        label: '굵게',
+        click: function () {
+          console.log('굵게')
+        }
+      },
+      {
+        label: '밑줄',
+        click: function () {
+          console.log('밑줄')
+        }
+      },
+      {
+        label: '취소선',
+        click: function () {
+          console.log('취소선')
+        }
+      },
+      {
+        label: '아래첨자',
+        click: function () {
+          console.log('아래첨자')
+        }
+      },
+      {
+        label: '윗첨자',
+        click: function () {
+          console.log('윗첨자')
+        }
+      },
+      {
+        label: '순서 없는 목록',
+        click: function () {
+          console.log('순서 없는 목록')
+        }
+      },
+      {
+        label: '순서 있는 목록',
+        click: function () {
+          console.log('순서 있는 목록')
+        }
+      },
+      {
+        label: '이미지',
+        click: function () {
+          console.log('이미지')
+        }
+      },
+      {
+        label: '링크',
+        click: function () {
+          console.log('링크')
+        }
+      },
+      {
+        label: '표',
+        click: function () {
+          console.log('표')
+        }
+      },
+    ],
+  },
+  {
+    label: '스타일',
+    submenu: [
+      {
+        label: '왼쪽 정렬',
+        click: function () {
+          console.log('왼쪽 정렬')
+        }
+      },
+      {
+        label: '가운데 정렬',
+        click: function () {
+          console.log('가운데 정렬')
+        }
+      },
+      {
+        label: '오른쪽 정렬',
+        click: function () {
+          console.log('오른쪽 정렬')
+        }
+      },
+      {
+        label: '양쪽 정렬',
+        click: function () {
+          console.log('양쪽 정렬')
+        }
+      },
+      {
+        label: '글자색 적용',
+        click: function () {
+          console.log('글자색 적용')
+        }
+      },
+      {
+        label: '배경색 적용',
+        click: function () {
+          console.log('배경색 적용')
+        }
+      },
+      {
+        label: '글꼴',
+        click: function () {
+          console.log('글꼴')
+        }
+      },
+      {
+        label: '글자크기 축소',
+        click: function () {
+          console.log('글자크기 축소')
+        }
+      },
+      {
+        label: '글자크기 확대',
+        click: function () {
+          console.log('글자크기 확대')
+        }
+      },
+      {
+        label: '나만의 style',
+        click: function () {
+          console.log('나만의 style')
+        }
+      },
+    ],
+  },
+  {
+    label: '도움말',
+    submenu: [
+      {
+        label: 'editor 사용 설명서 보기',
+        click: function () {
+          console.log('editor 사용 설명서 보기')
+        }
+      },
+    ],
+  },
 ];
 const menu = Menu.buildFromTemplate(template)
 
@@ -31,14 +321,13 @@ async function createWindow() {
   const win = new BrowserWindow({
     width: 950,
     height: 700,
-    autoHideMenuBar: true,
+    // autoHideMenuBar: true,
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION
     }
   })
-
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
     await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
--- epub_editor/src/components/mainpage/Directory.vue
+++ epub_editor/src/components/mainpage/Directory.vue
@@ -27,6 +27,21 @@ import * as edit from '@/functions/edit.js';
 const { dialog } = require('electron').remote;
 const fs = require('fs');
 
+// const {remote} = require('electron')
+// const {Menu, MenuItem} = remote
+
+// const menu = new Menu()
+// menu.append(new MenuItem({label: 'MenuItem1', click() { console.log('item 1 clicked') }}))
+// menu.append(new MenuItem({type: 'separator'}))
+// menu.append(new MenuItem({label: 'MenuItem2', type: 'checkbox', checked: true}))
+
+// window.addEventListener('contextmenu', (e) => {
+//   e.preventDefault()
+//   menu.popup(remote.getCurrentWindow())
+// }, false)
+
+
+
 export default {
   name: 'Directory',
   watch: {
