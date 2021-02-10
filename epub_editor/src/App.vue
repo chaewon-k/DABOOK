fe6c773a2dbbac73ce87b44ae74479dc3c344a77
Jubin Jang <sunshine0429@snu.ac.kr>
2021-02-10 14:35:04 +0900

    edit : macOS 빌드 버전 수정
    - 0.1.2 버전
    
    
    Former-commit-id: 5dd5340395b7810c513b8f63c2b9354baf8a9a31

--- epub_editor/src/App.vue
+++ epub_editor/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app id="inspire">
+  <v-app>
     <v-main>
       <Editor />
     </v-main>
--- epub_editor/src/background.js
+++ epub_editor/src/background.js
@@ -3,9 +3,11 @@
 import { app, protocol, Menu, BrowserWindow } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
+const { dialog } = require('electron')
 const isDevelopment = process.env.NODE_ENV !== 'production'
 const isMac = process.platform === 'darwin'
 let win;
+let menuEnabled = true;
 
 const template = [
   // { role: 'appMenu' }
@@ -30,37 +32,43 @@ const template = [
         label: '이북 생성하기',
         click: function () {
           win.webContents.send('fileTab', 0);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '이북 불러오기',
         click: function () {
           win.webContents.send('fileTab', 1);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '저장하기',
         click: function () {
           win.webContents.send('fileTab', 2);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '이북 미리보기',
         click: function () {
           win.webContents.send('fileTab', 3);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: 'ePub으로 내보내기',
         click: function () {
           win.webContents.send('fileTab', 4);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '목차 추가하기',
         click: function () {
           win.webContents.send('fileTab', 5);
-        }
+        },
+        enabled: menuEnabled
       },
     ],
   },
@@ -71,43 +79,50 @@ const template = [
         label: '실행취소',
         click: function () {
           win.webContents.send('editTab', 0);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '되돌리기',
         click: function () {
           win.webContents.send('editTab', 1);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '잘라내기',
         click: function () {
           win.webContents.send('editTab', 2);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '복사하기',
         click: function () {
           win.webContents.send('editTab', 3);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '붙여넣기',
         click: function () {
           win.webContents.send('editTab', 4);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '찾기',
         click: function () {
           win.webContents.send('editTab', 5);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '바꾸기',
         click: function () {
           win.webContents.send('editTab', 6);
-        }
+        },
+        enabled: menuEnabled
       },
       // {
       //   role: 'toggleDevTools'
@@ -121,37 +136,43 @@ const template = [
         label: '제목1',
         click: function () {
           win.webContents.send('toolsTab', 1);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '제목2',
         click: function () {
           win.webContents.send('toolsTab', 2);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '제목3',
         click: function () {
           win.webContents.send('toolsTab', 3);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '제목4',
         click: function () {
           win.webContents.send('toolsTab', 4);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '제목5',
         click: function () {
           win.webContents.send('toolsTab', 5);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '제목6',
         click: function () {
           win.webContents.send('toolsTab', 6);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         type: 'separator'
@@ -160,91 +181,106 @@ const template = [
         label: '이탤릭체',
         click: function () {
           win.webContents.send('toolsTab', 'Italic');
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '줄 바꿈',
         click: function () {
           win.webContents.send('toolsTab', 'Enter');
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '선',
         click: function () {
           win.webContents.send('toolsTab', 'LineTag');
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '인용구',
         click: function () {
           win.webContents.send('toolsTab', 'BlockquoteTag');
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '인용문',
         click: function () {
           win.webContents.send('toolsTab', 'CiteTag');
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '굵게',
         click: function () {
           win.webContents.send('toolsTab', 'BoldTag');
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '밑줄',
         click: function () {
           win.webContents.send('toolsTab', 'UnderlineTag');
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '취소선',
         click: function () {
           win.webContents.send('toolsTab', 'MediumlineTag');
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '아래첨자',
         click: function () {
           win.webContents.send('toolsTab', 'SubscriptTag');
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '윗첨자',
         click: function () {
           win.webContents.send('toolsTab', 'SuperscriptTag');
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '순서 없는 목록',
         click: function () {
           win.webContents.send('toolsTab', 'UnorderedListTag');
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '순서 있는 목록',
         click: function () {
           win.webContents.send('toolsTab', 'OrderedListTag');
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '이미지',
         click: function () {
           win.webContents.send('toolsTab', 'ImageTag');
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '링크',
         click: function () {
           win.webContents.send('toolsTab', 'Link');
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '표',
         click: function () {
           win.webContents.send('toolsTab', 'Table');
-        }
+        },
+        enabled: menuEnabled
       },
     ],
   },
@@ -255,85 +291,99 @@ const template = [
         label: '왼쪽 정렬',
         click: function () {
           win.webContents.send('styleTab', 0);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '가운데 정렬',
         click: function () {
           win.webContents.send('styleTab', 1);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '오른쪽 정렬',
         click: function () {
           win.webContents.send('styleTab', 2);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '양쪽 정렬',
         click: function () {
           win.webContents.send('styleTab', 3);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '글자색 적용',
         click: function () {
           win.webContents.send('styleTab', 4);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '배경색 적용',
         click: function () {
           win.webContents.send('styleTab', 5);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '글꼴',
         click: function () {
           win.webContents.send('styleTab', 6);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '글자크기 축소',
         click: function () {
           win.webContents.send('styleTab', 7);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '글자크기 확대',
         click: function () {
           win.webContents.send('styleTab', 8);
-        }
+        },
+        enabled: menuEnabled
       },
       {
         label: '나만의 style',
         click: function () {
           win.webContents.send('styleTab', 9);
-        }
+        },
+        enabled: menuEnabled
       },
     ],
   },
   {
     label: '윈도우',
-    submenu: [
-      { 
-        label: '최소화',
-        role: 'minimize'
-      },
-      { role: 'zoom' },
-      ...(isMac ? [
-        { type: 'separator' },
-        { role: 'front' },
-        { type: 'separator' },
-        { role: 'window' }
-      ] : [
-        { 
-          label: '닫기',
-          role: 'close'
-        }
-      ])
-    ]
+    role: 'windowMenu'
   },
+  // {
+  //   label: '윈도우',
+  //   submenu: [
+  //     { 
+  //       label: '최소화',
+  //       role: 'minimize'
+  //     },
+  //     { role: 'zoom' },
+  //     ...(isMac ? [
+  //       { type: 'separator' },
+  //       { role: 'front' },
+  //       { type: 'separator' },
+  //       { role: 'window' }
+  //     ] : [
+  //       { 
+  //         label: '닫기',
+  //         role: 'close'
+  //       }
+  //     ])
+  //   ]
+  // },
   {
     label: '도움말',
     submenu: [
@@ -341,7 +391,8 @@ const template = [
         label: 'editor 사용 설명서 보기',
         click: function () {
           win.webContents.send('helpTab', 0);
-        }
+        },
+        enabled: menuEnabled
       },
     ],
   },
@@ -378,15 +429,56 @@ async function createWindow() {
     console.log('isMac')
     win.webContents.send('isMac', true);
   }
+  let close = false    
+
+  win.on('close', (ev) => {
+    if (close === false) {
+      ev.preventDefault()
+      dialog.showMessageBox({
+        type: 'warning',
+        buttons: ['아니오', '네'],
+        message: '변경 내역을 모두 저장하셨나요?',
+        cancelId: 0,
+        defaultId: 1,
+        noLink: true
+      }).then((val) => {
+        if (val.response === 0) {
+          // Cancel the close process
+        } else if (win) {
+          close = true
+          app.quit()
+        }
+      })
+    }
+  })
+  // win.addEventListenter('beforeunload', function (event) {
+  //   console.log('I do not want to be closed')
+  //   const options = {
+  //     type: 'question',
+  //     message: '변경 사항을 모두 저장하셨나요?',
+  //     buttons: ['네', '아니오'],
+  //   }
+  //   const result = dialog.showMessageBoxSync(options);
+  //   event.returnValue = result;
+  // })
 }
+
+// app.on('before-quit', () => {
+//   const options = {
+//     type: 'question',
+//     message: '변경 사항을 모두 저장하셨나요?',
+//     buttons: ['네', '아니오'],
+//   }
+//   const result = dialog.showMessageBoxSync(options);
+// })
+
+
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+  app.quit()
 })
 
 app.on('activate', () => {
