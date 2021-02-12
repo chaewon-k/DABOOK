d65cda433cf0b861c30e70dae54018a41867516e
최낙훈 <cnh6123@naver.com>
2021-02-12 16:16:33 +0900

    edit : User,Epub,File을 관계형 데이터베이스로 연결
    db model들을 관계형 데이터베이스처럼 연결
    
    
    Former-commit-id: 638ff912393343ad8e9268f2947627e47f75b334

--- epub_server/src/main/java/com/ssafy/epub/repository/EpubRepository.java
+++ epub_server/src/main/java/com/ssafy/epub/repository/EpubRepository.java
@@ -1,9 +1,12 @@
 package com.ssafy.epub.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.ssafy.epub.model.Epub;
 
 public interface EpubRepository extends MongoRepository<Epub, String>{
-
+	List<Epub> findByEpubName(String epubName);
+	Epub findBy_id(String _id);
 }
--- epub_server/src/main/java/com/ssafy/epub/repository/UserRepository.java
+++ epub_server/src/main/java/com/ssafy/epub/repository/UserRepository.java
@@ -1,12 +1,10 @@
 package com.ssafy.epub.repository;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.ssafy.epub.model.User;
 
 public interface UserRepository extends MongoRepository<User, String>{
-	List<User> findByEmail(String email);
-	List<User> findByNickname(String nickname);
+	User findByEmail(String email);
+	User findByNickname(String nickname);
 }
