975804284fd301f54acbbe7cf2089933e893c02a
최낙훈 <cnh6123@naver.com>
2021-02-14 15:57:56 +0900

    edit : delete method upload
    delete method upload
    
    
    Former-commit-id: 95a2d78b0838315588bc33ed12c5bd41f3a85e23

--- epub_server/src/main/java/com/ssafy/epub/controller/UserController.java
+++ epub_server/src/main/java/com/ssafy/epub/controller/UserController.java
@@ -178,8 +178,7 @@ public class UserController {
 	@DeleteMapping("/user")
 	@ApiOperation(value = "deleteUser")
 	@ApiImplicitParams({ @ApiImplicitParam(name = "user", value = "회원 객체", required = true, dataType = "User") })
-	public ResponseEntity<Boolean> deleteUser(@RequestBody Map<String, String> req) {
-		String email = req.get("email");
+	public ResponseEntity<Boolean> deleteUser(String email) {
 		User user = userRepository.findByEmail(email);
 		
 		userRepository.delete(user);
