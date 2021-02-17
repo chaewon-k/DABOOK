d4e8b8d8b6d08a69451dc2e7e0549535ebda462f
최낙훈 <cnh6123@naver.com>
2021-02-17 14:26:18 +0900

    edit : 파일 업로드 다운로드 방식 변경
    파일 업로드 다운로드 방식 변경
    
    
    Former-commit-id: caf59f9142abd426527eee6ebc986e8f66d285d2

--- epub_server/src/main/java/com/ssafy/epub/controller/EpubController.java
+++ epub_server/src/main/java/com/ssafy/epub/controller/EpubController.java
@@ -1,12 +1,16 @@
 package com.ssafy.epub.controller;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -31,6 +35,7 @@ import com.ssafy.epub.model.User;
 import com.ssafy.epub.repository.EpubRepository;
 import com.ssafy.epub.repository.FileRepository;
 import com.ssafy.epub.repository.UserRepository;
+import com.ssafy.epub.service.EpubService;
 
 import io.swagger.annotations.ApiOperation;
 
@@ -45,17 +50,19 @@ public class EpubController {
 	private EpubRepository epubRepository;
 	@Autowired
 	private FileRepository fileRepository;
+	@Autowired
+	private EpubService epubService;
 	@Value("${spring.file.location}")
 	private String storagePath;
-	
+
 	@GetMapping("/file")
 	@ApiOperation(value = "getAllFiles", produces = MediaType.TEXT_PLAIN_VALUE)
 	public ResponseEntity<List<FileDTO>> getAllFiles(String id) {
 		List<FileDTO> fileList = epubRepository.findBy_id(id).getFileList();
-		
-		return new ResponseEntity<>(fileList,HttpStatus.OK);
+
+		return new ResponseEntity<>(fileList, HttpStatus.OK);
 	}
-	//, consumes = { MediaType.MULTIPART_FORM_DATA_VALUE }
+	
 	@PostMapping(value = "/upload")
 	@ApiOperation(value = "upload file")
 	public ResponseEntity<Boolean> upload(FileVO fileVo) {
@@ -131,26 +138,140 @@ public class EpubController {
 		
 		return new ResponseEntity<>(true,HttpStatus.OK);
 	}
-	
+
+
+	// , consumes = { MediaType.MULTIPART_FORM_DATA_VALUE }
+	@PostMapping(value = "/upload/zip")
+	@ApiOperation(value = "upload file")
+	public ResponseEntity<Boolean> uploadZip(FileVO fileVo) {
+		// 압축 파일 저장 경로
+		String unZipFilePath = storagePath + "/" + fileVo.getEmail() + "/" + fileVo.getEpubName() + "/";
+		// + fileVo.getEpubName() + "/";
+		MultipartFile mfFile = fileVo.getFile();
+		String epubName = fileVo.getEpubName();
+
+		// 압축 파일 경로
+		File zipFile = new File(unZipFilePath + mfFile.getOriginalFilename());
+		
+		
+		FileInputStream fis = null;
+		ZipInputStream zis = null;
+		ZipEntry zipentry = null;
+
+		try {
+			// zipFileName을 통해서 폴더 만들기
+			if (zipFile.getParentFile().mkdirs()) {
+				System.out.println("압축 폴더를 생성했습니다");
+			}
+
+			// 파일 스트림
+			try {
+				zipFile.createNewFile();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			
+			try {
+				// 업로드 임시파일 -> 물리파일 덮어쓰기
+				mfFile.transferTo(zipFile);
+			}
+			catch(IOException ie) {
+				// 덮어쓰기 중 오류
+				ie.printStackTrace();
+				return ResponseEntity.status(HttpStatus.CONFLICT).build();
+			}
+			//압축 풀기
+			fis = new FileInputStream(zipFile);
+
+			// Zip 파일 스트림
+			zis = new ZipInputStream(fis, Charset.forName("EUC-KR"));
+
+			// 압축되어 있는 ZIP 파일의 목록 조회
+			while ((zipentry = zis.getNextEntry()) != null) {
+				String fileName = zipentry.getName();
+				System.out.println("filename(zipentry.getName()) => " + fileName);
+				File file = new File(unZipFilePath, fileName);
+
+				// entry가 폴더면 폴더 생성
+				if (zipentry.isDirectory()) {
+					System.out.println("zipentry가 디렉토리입니다.");
+					file.mkdirs();
+				} else {
+					// 파일이면 파일 만들기
+					System.out.println("zipentry가 파일입니다.");
+					try {
+						epubService.createFile(file, zis);
+						//경로
+						String path = file.getParent();
+						//파일 이름 fileName
+						
+						path = path.replace(storagePath + "\\" +fileVo.getEmail() ,"");
+						path = path.replace("\\", "/");
+						if(path.length() == 0)
+							path = "/";
+						System.out.println(path);
+						
+						epubService.fildDBFunc(fileVo.getEmail(),path,epubName,fileName);
+					} catch (Throwable e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		} catch (Exception e) {
+		} finally {
+			if (zis != null) {
+				try {
+					zis.close();
+				}catch (IOException e) {
+					
+				}
+			}
+			if (fis != null) {
+				try {
+					fis.close();
+				} catch (IOException e) {
+					
+				}
+			}
+		}
+		
+		zipFile.delete();
+		return new ResponseEntity<>(true, HttpStatus.OK);
+	}
+
 	@GetMapping("/download")
 	@ApiOperation(value = "download file")
-	public ResponseEntity<Resource> download(@RequestParam String email, @RequestParam String epubName, @RequestParam String path, @RequestParam String fileName) throws IOException {
-		Path localPath = Paths.get(storagePath + "/" + email + "/" + epubName + path + "/" + fileName);
-		String contentType = Files.probeContentType(localPath);
+	public ResponseEntity<Resource> download(@RequestParam String email, @RequestParam String epubName
+			) throws IOException {
+		Path localPath = Paths.get(storagePath + "/" + email + "/" + epubName);
 		
+		String zipFileName = epubName + ".zip";
+		
+		try {
+			System.out.println(localPath.toString());
+			epubService.compress(localPath.toString(), localPath.toString(), "");
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String contentType = Files.probeContentType(localPath);
+
 		HttpHeaders headers = new HttpHeaders();
 		headers.add(HttpHeaders.CONTENT_TYPE, contentType);
-		headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"");
+		headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + zipFileName + "\"");
 		
-		Resource resource = new InputStreamResource(Files.newInputStream(localPath));
+		Path zipPath = Paths.get(storagePath + "/" + email + "/" + zipFileName);
+		
+		Resource resource = new InputStreamResource(Files.newInputStream(zipPath));
 		return new ResponseEntity<>(resource, headers, HttpStatus.OK);
 	}
-	
+
 	@GetMapping("/epub/file/list")
 	@ApiOperation(value = "해당 epub에 대한 fileList 반환", produces = MediaType.TEXT_PLAIN_VALUE)
 	public ResponseEntity<List<FileDTO>> getFileList(@RequestParam String id) {
 		List<FileDTO> fileList = epubRepository.findBy_id(id).getFileList();
-		
+
 		return new ResponseEntity<>(fileList, HttpStatus.OK);
 	}
 }
